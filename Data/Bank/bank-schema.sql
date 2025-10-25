c89f1873918e68141812e89d1e3564c820662948
Pablo Lorenzo <pabloernesto802@gmail.com>
2025-10-25 13:43:59 -0400

    feat: added database triggers

--- Data/Bank/bank-schema.sql
+++ Data/Bank/bank-schema.sql
@@ -26,6 +26,55 @@ CREATE TABLE "transfers" (
 	FOREIGN KEY ("receiver_id") REFERENCES "accounts"("account_id")
 );
 
+CREATE TABLE "logs" (
+	"log_id" INTEGER,
+	"account_id" INTEGER NOT NULL DEFAULT 0,
+	"information" VARCHAR(250) NOT NULL,
+	"date" DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY ("log_id"),
+	FOREIGN KEY ("account_id") REFERENCES "accounts"("account_id") ON DELETE SET DEFAULT
+);
+
+-- Triggers
+CREATE TRIGGER "account_egress_transaction"
+AFTER INSERT ON "transactions"
+FOR EACH ROW WHEN NEW."type" = 'WITHDRAWAL'
+BEGIN
+	INSERT INTO "logs" ("account_id", "information")
+	VALUES (
+		NEW."account_id", 
+		NEW."account_id" || " withdraw $" || NEW."amount" || " from its account new balance: " || (SELECT "Balance" FROM "Balance" WHERE "account_id" = NEW."account_id")
+	);
+END;
+
+CREATE TRIGGER "account_ingress_transaction"
+AFTER INSERT ON "transactions"
+FOR EACH ROW WHEN NEW."type" = 'WITHDRAWAL'
+BEGIN
+	INSERT INTO "logs" ("account_id", "information")
+	VALUES (
+		NEW."account_id", 
+		NEW."account_id" || " deposited $" || NEW."amount" || " from its account new balance: " || (SELECT "Balance" FROM "Balance" WHERE "account_id" = NEW."account_id")
+	);
+END;
+
+CREATE TRIGGER "account_transfers"
+AFTER INSERT ON "transfers"
+FOR EACH ROW
+BEGIN
+	INSERT INTO "logs" ("account_id", "information")
+	VALUES (
+		NEW."sender_id", 
+		NEW."sender_id" || " transfered $" || NEW."amount" || " to: " || NEW."receiver_id" || " new balance: " || (SELECT "Balance" FROM "Balance" WHERE "account_id" = NEW."sender_id")
+	);
+
+	INSERT INTO "logs" ("account_id", "information")
+	VALUES (
+		NEW."receiver_id", 
+		NEW."receiver_id" || " received from a transfer $" || NEW."amount" || " from: " || NEW."sender_id" || " new balance: " || (SELECT "Balance" FROM "Balance" WHERE "account_id" = NEW."receiver_id")
+	);
+END;
+
 -- VIEWS
 CREATE VIEW "Balance" AS
 WITH
@@ -36,7 +85,6 @@ debit AS (
 	FROM "accounts"
 	LEFT JOIN "transactions" ON "transactions"."account_id" = "accounts"."account_id"AND type = 'DEPOSIT'
 	LEFT JOIN "transfers" 	 ON "transfers"."receiver_id" = "accounts"."account_id"
-	WHERE "accounts"."account_id" = 1
 	GROUP BY "accounts"."account_id"
 ),
 credit AS (
@@ -46,14 +94,13 @@ credit AS (
  	FROM "accounts"
 	LEFT JOIN "transactions" ON "transactions"."account_id" = "accounts"."account_id"AND type = 'WITHDRAWAL'
 	LEFT JOIN "transfers" 	 ON "transfers"."sender_id" = "accounts"."account_id"
-	WHERE "accounts"."account_id" = 1
 	GROUP BY "accounts"."account_id"
 )
 SELECT
+"debit"."AccountId" AS "account_id",
 ("debit"."amount" - "credit"."amount") AS 'Balance'
 FROM "debit"
 JOIN "credit" ON "debit"."AccountId" = "credit"."AccountId";
-
 
 -- Data Seed
 
