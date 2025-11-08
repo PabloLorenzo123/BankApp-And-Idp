6ce93121068801ef253ea8da1d3d0bd2962f220a
Pablo Lorenzo <pabloernesto802@gmail.com>
2025-11-08 13:28:32 -0400

    feat: added host builder

---
+++ BankApp/AppMenu.cs
@@ -0,0 +1,84 @@
+﻿using Bank;
+using IDP;
+using Memento;
+using Microsoft.Extensions.Hosting;
+
+namespace BankApp
+{
+    internal class AppMenu(IDPApi idpApi, BankApi bankApi, Authentication authentication): IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1) Register an user in the Bank IDP.");
+                Console.WriteLine("2) Use Bank App.");
+                Console.WriteLine("3) Exit.");
+                int option = Utils.PromptNumber("Choose an option: ");
+
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        idpApi.SignUp();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        UseBankApp();
+                        break;
+                    case 3:
+                        return StopAsync(cancellationToken);
+                    default:
+                        Console.WriteLine("Provide a correct option.");
+                        break;
+                }
+            }
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Thanks for using this app.");
+            return Task.CompletedTask;
+        }
+
+        private void UseBankApp()
+        {
+            Console.Clear();
+
+            var identityToken = authentication.DeseralizeToken(authentication.GetIdentityTokenUsingAuthCodeFlow());
+            var bankAccount = bankApi.GetBankAccount(identityToken.Payload.Sub);
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine($"\nWelcome to the Bank App You're logged in as ID{bankAccount.AccountId}");
+            Console.WriteLine("\nBalance: " + bankApi.GetBalance(bankAccount));
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("1) Transfer money.");
+                Console.WriteLine("2) See logs.");
+                Console.WriteLine("3) Exit.");
+                int option = Utils.PromptNumber("Choose an option: ");
+
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        bankApi.DoTransaction(bankAccount);
+                        break;
+                    case 2:
+                        bankApi.SeeLogs();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        return;
+                    default:
+                        Console.WriteLine("Provide a correct option.");
+                        break;
+                }
+            }
+        }
+    }
+}
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Bank;
+using BankApp;
 using Data;
 using IDP;
 using IDP.Repositories;
@@ -6,104 +7,31 @@ using IDP.Services;
 using Memento;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 public static class Program
 {
-    private static IServiceProvider _services = new ServiceCollection().BuildServiceProvider();
     public static void Main()
     {
-        _services = ConstructDiContainer();
-
-        while (true)
-        {
-            Console.Clear();
-            Console.WriteLine("1) Register an user in the Bank IDP.");
-            Console.WriteLine("2) Use Bank App.");
-            Console.WriteLine("3) Exit.");
-            int option = Utils.PromptNumber("Choose an option: ");
-
-            switch (option)
+        var builder = new HostBuilder()
+            .ConfigureAppConfiguration(configuration =>
             {
-                case 1:
-                    Console.Clear();
-                    RegisterUser();
-                    break;
-                case 2:
-                    Console.Clear();
-                    UseBankApp();
-                    break;
-                case 3:
-                    return;
-                default:
-                    Console.WriteLine("Provide a correct option.");
-                    break;
-            }
-        }
-    }
-
-    private static void RegisterUser() => _services.GetRequiredService<IDPApi>().SignUp();
-    
-    private static void UseBankApp()
-    {
-        Console.Clear();
-        var authentication = _services.GetRequiredService<Authentication>();
-        var bankApi = _services.GetRequiredService<BankApi>();
-
-        var identityToken = authentication.DeseralizeToken(authentication.GetIdentityTokenUsingAuthCodeFlow());
-        var bankAccount = bankApi.GetBankAccount(identityToken.Payload.Sub);
-        Console.WriteLine("Press any key to continue.");
-        Console.ReadLine();
-
-        Console.Clear();
-        Console.WriteLine($"\nWelcome to the Bank App You're logged in as ID{bankAccount.AccountId}");
-        Console.WriteLine("\nBalance: " + bankApi.GetBalance(bankAccount));
-
-        while (true)
-        {
-            Console.WriteLine();
-            Console.WriteLine("1) Transfer money.");
-            Console.WriteLine("2) See logs.");
-            Console.WriteLine("3) Exit.");
-            int option = Utils.PromptNumber("Choose an option: ");
-
-            switch (option)
+                configuration.AddJsonFile("settings.json", optional: false);
+            })
+            .ConfigureServices(services =>
             {
-                case 1:
-                    Console.Clear();
-                    bankApi.DoTransaction(bankAccount);
-                    break;
-                case 2:
-                    bankApi.SeeLogs();
-                    break;
-                case 3:
-                    Console.Clear();
-                    return;
-                default:
-                    Console.WriteLine("Provide a correct option.");
-                    break;
-            }
-        }
-    }
+                services.AddScoped<ConnectionFactory>();
+                services.AddScoped<UsersRepository>();
+                services.AddScoped<IDPApi>();
+                services.AddScoped<BankApi>();
+                services.AddScoped<OAuthService>();
+                services.AddScoped<TokenGenerator>();
+                services.AddScoped<Authentication>();
 
-    private static ServiceProvider ConstructDiContainer()
-    {
-        var services = new ServiceCollection();
+                services.AddHostedService<AppMenu>();
+            });
 
-        // Configuration.
-        IConfiguration configuration = new ConfigurationBuilder()
-            .AddJsonFile("settings.json")
-            .Build();
-        services.AddSingleton(configuration);
-
-        // Services.
-        services.AddScoped<ConnectionFactory>();
-        services.AddScoped<UsersRepository>();
-        services.AddScoped<IDPApi>();
-        services.AddScoped<BankApi>();
-        services.AddScoped<OAuthService>();
-        services.AddScoped<TokenGenerator>();
-        services.AddScoped<Authentication>();
-
-        return services.BuildServiceProvider();
+        var app = builder.Build();
+        app.Run();
     }
 }
